2bacbf219793dc4eae2fbecd5f8e719a5fecf3d8
Dan Barrio <11011568+danbarrio@users.noreply.github.com>
2018-10-15 19:18:55 -0700

    Exercise 02 - Variables and Functions

---
+++ Unity_CSharp/Assets/-[SCENES]-/Beginner-Gameplay-Scripting/02_Variables-and-Functions/VariablesAndFunctions.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    public int myInt = 5;
+    public int multValue = 1;
+    int result;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            result = MultiplyByValue(myInt);
+            Debug.Log("Result = " + result);
+        }
+    }
+
+    int MultiplyByValue(int number)
+    {
+        int returnValue;
+        returnValue = number * multValue;
+        return returnValue;
+    }
+
+
+    /*
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+
+    int MultiplyByTwo(int number)
+    {
+        int ret;
+        ret = number * 2;
+        return ret;
+    }
+
+    */
+
+}
